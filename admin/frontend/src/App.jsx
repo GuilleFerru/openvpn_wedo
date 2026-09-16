3360dc21a6dcd8c77870618955c70829ffc2481b
Nja0 <emimonson8@gmail.com>
2026-09-16 09:47:39 -0300

    feat: Cambiar paginacion a 8 items por pagina

--- admin/frontend/src/App.jsx
+++ admin/frontend/src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
   const [pageActive, setPageActive] = useState(1);
   const [pageClients, setPageClients] = useState(1);
   const [pageGroups, setPageGroups] = useState(1);
-  const ITEMS_PER_PAGE = 10;
+  const ITEMS_PER_PAGE = 8;
 
   const [loading, setLoading] = useState(true);
   
